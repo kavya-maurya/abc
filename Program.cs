3a9e17bd419146741227cf61668281c1a56af4c9
kavya <kavyamaurya269@gmail.com>
2025-06-21 11:23:35 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace newfolder {
+    class Program
+    { 
+        static void Main(string[]args)
+    {   
+       //ye ek name enter karwane ka code hai 
+        // Console.WriteLine("Hey There!! Who Are You ?");
+        // string name = Console.ReadLine();
+        // Console.WriteLine($"Hey {name}, Thanks for coming");
+
+
+// Console.WriteLine("Enter the first value please");
+// string firstValue = Console.ReadLine();
+
+// Console.WriteLine("Enter the second value pls");
+// string secondValue = Console.ReadLine();
+
+// string sum = firstValue+secondValue;
+
+// Console.WriteLine($"Your sum is : {sum}");
+
+
+
+
+// Console.WriteLine("Enter the first value please");
+// int firstValue = Convert.ToInt32(Console.ReadLine()) ;
+
+// Console.WriteLine("Enter the second value pls");
+// int secondValue = Convert.ToInt32(Console.ReadLine());
+
+// int sum = firstValue+secondValue;
+// Console.WriteLine($"Your sum is : {sum}");
+
+
+// Console.WriteLine("enter the first value ");
+// int firstValue = Convert.ToInt32(Console.ReadLine());
+
+
+// Console.WriteLine("enter the second value");
+// int secondValue = Convert.ToInt32(Console.ReadLine());
+
+
+// Console.WriteLine("enter the third value");
+// int thirdValue = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("enter the fourth value");
+// int fourthValue= Convert.ToInt32(Console.ReadLine());
+
+// int sum = firstValue+secondValue +thirdValue + fourthValue;
+// int difference = firstValue-secondValue-thirdValue -fourthValue;
+// int product = firstValue*secondValue *thirdValue* fourthValue;
+// int division = firstValue/secondValue; 
+
+
+
+// Console.WriteLine ($"for the given value , the sum is {sum} , the difference is { difference} ,the product is {product} and the value on division is {division}");
+
+//Conditional Statements Here.
+
+Console.WriteLine("Please provide a number");
+int firstNumber = int.Parse(Console.ReadLine());
+
+// if (firstNumber==45)
+// {
+//     Console.WriteLine($"You Entered {firstNumber}");
+// }
+
+// if (firstNumber>45)
+// {
+//     Console.WriteLine($"You Entered Greater than {firstNumber} > 45 is true");
+// }
+
+// if (firstNumber>30)
+// {
+//     Console.WriteLine($"You Entered Greater than {firstNumber} > 30 is true");
+// }
+
+// if (firstNumber<45)
+// {
+//     Console.WriteLine($"You Entered Lesser than {firstNumber}");
+// }
+
+
+if(firstNumber>45)
+{
+
+    if(firstNumber==70)
+    {
+Console.WriteLine($"You entered {firstNumber}");
+
+    }
+
+    if(firstNumber==80)
+    {
+Console.WriteLine($"You entered {firstNumber}");
+
+    }
+
+}
+
+else if(firstNumber<30)
+{
+Console.WriteLine($"You greater than {firstNumber} < 30");
+}
+
+else {
+Console.WriteLine($"Please give me greater than 45 value, I cant accept negative value");
+
+}
+
+
+
+
+
+    }
+
+    }
+
+
+}
+
+
